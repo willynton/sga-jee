99ea662b51957922cf6b772f506a4ea9ad2d0b51
Willynton Lopez Hincapie <willynton@gmail.com>
2020-11-13 14:30:02 +0100

    version 1.1

--- src/main/java/mx/com/gm/sga/cliente/ClientePersonaService.java
+++ src/main/java/mx/com/gm/sga/cliente/ClientePersonaService.java
@@ -25,6 +25,7 @@ public class ClientePersonaService {
                 System.out.println(persona);
             }
             
+            
             System.out.println("\n Fin de la llamada al EJB desde el cliente: ");
             
         } catch (NamingException ex) {
