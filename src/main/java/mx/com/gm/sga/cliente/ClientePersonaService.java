badced342d0f08dbd88a5afbac55459f7ca5851e
Willynton Lopez Hincapie <willynton@gmail.com>
2020-11-13 13:10:44 +0100

    1er proyecto y 1er commit en git

---
+++ src/main/java/mx/com/gm/sga/cliente/ClientePersonaService.java
@@ -0,0 +1,36 @@
+
+package mx.com.gm.sga.cliente;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.*;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.servicio.PersonaServiceRemote;
+
+
+public class ClientePersonaService {
+    public static void main(String[] args) {
+        
+        System.out.println("Iniciando llamada al EJB desde el cliente\n");
+        
+        try {
+            Context jndi = new InitialContext();
+            
+            PersonaServiceRemote personaService=(PersonaServiceRemote)jndi.lookup("java:global/sga-jee/PersonaServiceImpl!mx.com.gm.sga.servicio.PersonaServiceRemote");
+            
+            List<Persona> personas = personaService.listarPersonas();
+            
+            for(Persona persona: personas){
+                System.out.println(persona);
+            }
+            
+            System.out.println("\n Fin de la llamada al EJB desde el cliente");
+            
+        } catch (NamingException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+        
+    }
+}
